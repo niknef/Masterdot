b6737d407b1814124ec6507ea96bd57d872705c0
Nicolas Firpo <nicolas.firpo@davinci.edu.ar>
2023-11-07 18:50:33 -0300

    add all features

---
+++ js/juego.js
@@ -0,0 +1,197 @@
+/*
+* JS Para el juego Masterdots
+*
+*/
+//VARIABLES GLOBALES
+var iniciadoMarcado=false;
+var adyacentes=[];
+var idMarcados=[];
+var classMarcada;
+var tamanoPanel;
+var idInterval;
+
+/* INICIALIZACIÓN DEL PANEL */
+/**
+ * Devuelve un numero random entre 0 y max
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+ * @param  {} max
+ */
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+  
+/**
+ * Funcion que rellena nick y src de avatar
+ */
+function rellenarFormularioUsuario(){
+    document.getElementById("nick").value=nick;
+    document.getElementById("avatarImg").src=avatarImg;
+    tamanoPanel=parseInt(tamano);
+}
+
+/**
+ * Funcion que:
+ *  1.- Rellena el nick
+ *  2.- Rellena el avatar
+ *  3.- Pinta de forma automática el panel de juego
+ */
+function pintarPanelJuego(){
+    document.getElementById("juego").style.gridTemplateColumns="repeat("+tamano+", 1fr)"
+    document.getElementById("juego").style.gridTemplateRows="repeat("+tamano+", 1fr)"
+    //Elementos de forma automatica
+    let items="";
+    let color=["rojo","verde"];
+    let colorRnd=0;
+    for (let index = 0; index < (parseInt(tamano)*parseInt(tamano)); index++) {
+        if(index%2>0) colorRnd=getRandomInt(2);
+        items+=`<div class="containerItem"><div id="${index}" class="item ${color[colorRnd]}"></div></div>`;
+    }
+    document.getElementById("juego").innerHTML=items;
+}
+
+/**
+ * Calcula el array de los adyacentes
+ * @param  {} idMarcado número marcado
+ */
+function calcularAdyacentes(idMarcado){
+    adyacentes=[];
+    //Adyacente superior
+    if((idMarcado-tamanoPanel)>=0) adyacentes.push(idMarcado-tamanoPanel);
+    //Adyacente inferior
+    if((idMarcado+tamanoPanel)<(tamanoPanel*tamanoPanel)) adyacentes.push(idMarcado+tamanoPanel);
+    //Adyacente izquierda
+    if((idMarcado%tamanoPanel)>0) adyacentes.push(idMarcado-1);
+    //Adyacente derecha
+    if(((idMarcado+1)%tamanoPanel)>0) adyacentes.push(idMarcado+1);
+
+    for (let index = 0; index < adyacentes.length; index++) {
+        console.log(adyacentes[index]);
+    }
+}
+
+/**
+ * Funcion que realiza el conteo hacia atrás del juego
+ */
+function cuentaAtras(){
+    let tmpoRestante=parseInt(document.getElementById("tmpo").value)-1;
+    document.getElementById("tmpo").value=tmpoRestante;
+    if(tmpoRestante==0){
+        clearInterval(idInterval);
+        //Finalizar todos los eventos
+        const items=document.getElementsByClassName('item');
+        for (let item of items) {
+            item.removeEventListener('mousedown',comenzarMarcar);
+            item.removeEventListener('mouseover',continuarMarcando);
+        }
+        document.removeEventListener('mouseup',finalizarMarcado);
+        //Cambiar z-index paneles
+        document.getElementById("juegoAcabado").classList.add('juegoAcabadoColor');
+        document.getElementById("juegoAcabado").style.zIndex="2";
+        document.getElementById("juego").style.zIndex="1";
+        document.getElementById("nuevaPartida").addEventListener("click",(e)=>location.reload());
+    }
+}
+
+/**
+ * Añadir los eventos al juego
+ */
+function programarEventosJuego(){
+    const items=document.getElementsByClassName('item');
+    for (let item of items) {
+        item.addEventListener('mousedown',comenzarMarcar);
+        item.addEventListener('mouseover',continuarMarcando);
+    }
+    document.addEventListener('mouseup',finalizarMarcado);
+    //Cuenta atrás
+    idInterval=setInterval(cuentaAtras,1000)
+}
+
+/* FUNCIONES DEL JUEGO */
+/**
+ * Iniciar el marcado de los dots
+ * @param  {} event
+ */
+function comenzarMarcar(event){
+    let item=event.target;
+    let containerItem=event.target.parentElement;
+    if(item.classList.contains('rojo')){
+        classMarcada='rojo';
+        containerItem.classList.add('rojo');
+    }
+    else{
+        classMarcada='verde';
+        containerItem.classList.add('verde');
+    }
+    if(!iniciadoMarcado) iniciadoMarcado=true;
+
+    //Guardo los marcados
+    idMarcados.push(parseInt(item.id));
+    //Comienzo a calcular adyacentes
+    calcularAdyacentes(parseInt(item.id));
+    console.log("Pinchado sobre un circulo");
+}
+
+/* FUNCIONES DEL JUEGO */
+/**
+ * Continuar el marcado de los dots
+ * @param  {} event
+ */
+ function continuarMarcando(event){
+    if(iniciadoMarcado){
+        let item=event.target;
+        let idNuevo=parseInt(item.id);
+        //Es adyacente?
+        if(adyacentes.includes(idNuevo)&&item.classList.contains(classMarcada))
+        {
+            let containerItem=event.target.parentElement;
+            if(item.classList.contains('rojo')) containerItem.classList.add('rojo');
+            else containerItem.classList.add('verde');
+            //Guardo los marcados
+            idMarcados.push(parseInt(item.id));
+            calcularAdyacentes(parseInt(item.id));
+        }
+
+    }
+    console.log("Pasando sobre un circulo");
+ }
+
+ /* FUNCIONES DEL JUEGO */
+/**
+ * Finalizaría el marcado de los dots
+ * @param  {} event
+ */
+ function finalizarMarcado(event){
+    iniciadoMarcado=false;
+    adyacentes=[];
+    //Añadiriamos puntuacion
+    const puntuacionInput=document.getElementById("puntuacion");
+    if(idMarcados.length>1){
+        puntuacionInput.value=parseInt(puntuacionInput.value)+idMarcados.length;
+    }
+    //Trabajar con los marcados
+    for (let index = 0; index < idMarcados.length; index++) {
+        //Capturar el objeto
+        let itemMarcado=document.getElementById(idMarcados[index]);
+        itemMarcado.parentElement.classList.remove(classMarcada);
+        //Cambiar el color de los objetos de forma rnd
+        let color=["rojo","verde"];
+        let colorRnd=getRandomInt(2);
+        itemMarcado.classList.remove(classMarcada);
+        itemMarcado.classList.add(color[colorRnd]);
+    }
+    idMarcados=[];
+    console.log("Finalizar el marcado");
+ }
+
+/*
+* MAIN
+*/
+
+//Capturamos Datos Usuaio
+getDatosUsuario();
+//Comprobamos los datos
+if(!comprobacionDatosUsuario()) location="index.html";
+//Rellenamos el formulario, panel y eventos
+rellenarFormularioUsuario();
+pintarPanelJuego();
+programarEventosJuego();
--- js/script.js
+++ js/script.js
@@ -1,25 +1,84 @@
-const nickInput=document.getElementById("nick");
-const sizeInput=document.getElementById("size");
-const formEntrada=document.getElementById("formEntrada");
-const error=document.getElementById("error");
+
+//Inicializacion de var,objetos, DOM
+var nickInput;
+var tamanoInput;
+var emailInput;
+var formEntrada;
+var error;
+var avatarItems;
+var itemImg;
+var avatarCont;
+
+//comprobamos si hay algun error de juego html
+
+
+
+/**
+ * Comprueba los datos correctos del formualrio de entrada
+ * @param  {EventObject} event Evento que salta al realizar submit
+ */
 
 function comprobarForm(event){
-    
-    if(nickInput.value.match(/(?<!\s)[0-9]/))
+    //Comprobar cambios
+    if(nickInput.value.match(/(?<!\S)[0-9]/))
     {
         nickInput.focus();
         event.preventDefault();
-        error.innerText="El campo del nick no debe comenzar con un numero";
+        error.innerText="El campo de nick no puede comenzar con un numero";
         return false;
-        
-    }else if(sizeInput.value=="0"){
-        sizeInput.focus();
+    }else if(tamanoInput.value=="0"){
+        tamanoInput.focus();
         event.preventDefault();
-        error.innerText="Elije un tamaño por favor";
+        error.innerText="Se debe seleccionar un tamaño de panel";
         return false;
     }
+    //Informacion es correcta
+    datosUsuario(nickInput,tamanoInput,emailInput,avatarCont);
+    historicoUsuarios(nombre, serie1,serie2,serie3);
     return true;
-    
 }
 
-formEntrada.addEventListener('submit',comprobarForm);
+function moviendoImg(event){
+    itemImg=event.target;
+    console.log(itemImg.src);
+}
+
+function cambiarImg(event){
+    avatarCont.src=itemImg.src;
+}
+
+/**
+ * Carga de objetos del DOM comprobaciones y eventos del formulario
+ */
+function domCargado(){
+    //Captura de todos los Elements
+    nickInput=document.getElementById("nick");
+    tamanoInput=document.getElementById("tamano");
+    emailInput=document.getElementById("email");
+    formEntrada=document.getElementById("formEntrada");
+    error=document.getElementById("error");
+
+    //Comprobar si hay algún error de juego.html
+    if(sessionStorage.getItem('error')!=null)
+    {
+        error.innerText=sessionStorage.getItem('error');
+        sessionStorage.removeItem('error');
+    }
+
+    formEntrada.addEventListener('submit',comprobarForm);
+
+    
+    //Eventos del D&D
+    avatarItems=document.getElementsByClassName("avatarImgItem");
+    for(let item of avatarItems){
+        item.addEventListener('dragstart',moviendoImg)
+    }
+    avatarCont=document.getElementById("avatarImg");
+    avatarCont.addEventListener('dragover',e=>{e.preventDefault()})
+    avatarCont.addEventListener('drop',cambiarImg)
+};
+
+//Inicio de carga de eventos
+document.addEventListener('DOMContentLoaded',domCargado);
+//Geolocalizacion
+datoGeolocalizacion();
